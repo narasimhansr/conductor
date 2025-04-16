62d0ac4b27825ec80c2372ad7021ea348ebed629
narasimhansr <vsnmarch1993@gmail.com>
2025-04-16 22:05:27 +0530

    Update WorkflowTestService.java

--- core/src/main/java/com/netflix/conductor/service/WorkflowTestService.java
+++ core/src/main/java/com/netflix/conductor/service/WorkflowTestService.java
@@ -2,7 +2,7 @@
  * Copyright 2023 Conductor Authors.
  * <p>
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
+ * the License. You may obtai sn a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
